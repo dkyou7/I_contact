181530f18ff151ec112a60ba75478b71d280686a
unknown <dlwlgus4553@gmail.com>
2019-11-09 20:49:03 +0900

    init url

--- I_contact/I_contact/urls.py
+++ I_contact/I_contact/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('login.urls')),
+    # path('each_punding/', include('each_punding.urls')),
+    path('reward/', include('reward.urls')),
+    path('board/', include('board.urls'))
+    
+
 ]
