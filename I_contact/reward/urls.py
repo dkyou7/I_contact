00a0fdd76fc48e90a598855f0dc8309971bc31bc
youdongkwan <dkyou7@naver.com>
2019-11-10 04:23:58 +0900

    card add

--- I_contact/reward/urls.py
+++ I_contact/reward/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
+    path('reward/',views.reward, name='reward'),
+    path('new', views.new, name = 'new_reward'),
+    path('create', views.create, name= 'create_reward'),
+    path('<int:reward_id>', views.detail , name = 'detail_reward'),
+
 ]
