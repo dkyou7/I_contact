4b298cef132e1551287a641f3ff0a1f8efd3f0fb
youdongkwan <dkyou7@naver.com>
2019-11-09 22:29:09 +0900

    merging

--- I_contact/I_contact/urls.py
+++ I_contact/I_contact/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import board.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', board.views.home, name ="home"),
+    path('board/', include('board.urls')),
+    path('login/', include('login.urls')),
+    path('reward/', include('reward.urls')),
+
 ]
