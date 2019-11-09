c3fa45f1db889ecc516dc43cd5eb45eb010129b5
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2019-11-09 21:26:01 +0900

    board에 crud 만들기

---
+++ I_contact/board/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('<int:board_id>', views.detail , name = 'detail'),
+    path('new', views.new, name = 'new'),
+    path('create', views.create, name= 'create'),
+    path('edit/<int:board_id>', views.edit, name ="edit"),
+    path('update/<int:board_id>', views.update, name = "update"),
+    path('delete/<int:board_id>', views.delete, name = "delete"),
+
+]
--- I_contact/board/views.py
+++ I_contact/board/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Board
+from django.utils import timezone
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    board = Board.objects
+    return render(request,'home.html',{'boards':board})
+
+def detail(request, board_id):
+    board_detail = get_object_or_404(Board, pk = board_id )
+    return render(request, 'detail.html',{'board':board_detail})
+
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    new_board = Board()
+    new_board.title = request.POST['title']
+    new_board.date = timezone.datetime.now()
+    new_board.body = request.POST['body']
+    new_board.save()
+    return redirect('/board/'+str(new_board.id))
+
+def edit(request,board_id):
+    edit_board = Board.objects.get(id=board_id)
+    return render(request, 'edit.html',{'board':edit_board})
+
+def update(request, board_id):
+    update_board = Board.objects.get (id = board_id)
+    update_board.title = request.POST['title']
+    update_board.body = request.POST['body']
+    update_board.save()
+    return redirect('/board/'+str(update_board.id))
+
+def delete(request, board_id):
+    delete_board = Board.objects.get(id=board_id)
+    delete_board.delete()
+    return redirect('home')
